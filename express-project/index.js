fe50e295604029ef5b5c075ca4b8190eeca1137e
ayushmish605 <mishraayush605@gmail.com>
2024-03-24 03:37:28 -0400

    work

--- express-project/index.js
+++ express-project/index.js
@@ -18,7 +18,7 @@ var params = {
 }
 liveServer.start(params);
 
-app.get('/home', function(req, res) {
+app.post('/', function(req, res) {
     request('http://127.0.0.1:5000/flask', function (error, response, body) {
         console.error('error:', error); // Print the error
         console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
