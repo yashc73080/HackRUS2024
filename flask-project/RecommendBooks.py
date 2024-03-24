9dc121d66f841544677c13505f5b609e10659705
Yash Chennawar <144838733+yashc73080@users.noreply.github.com>
2024-03-23 23:02:51 -0400

    some stuff

--- flask-project/RecommendBooks.py
+++ flask-project/RecommendBooks.py
@@ -6,15 +6,31 @@ df = pd.read_csv('flask-project/cleaned_goodreads_data.csv')
 
 
 # Function to return array of name, description, index for the first time
-
-def firstBookCalls():
+books_array = []
+def firstBookCall():
     random_indices = df.sample(n=12).index
     indices_array = random_indices.tolist()
 
-    books_array = []
     for i in indices_array:
         row = df.loc[i]
-        books_array.append([row['Book'], row['Author'], row['Description'], row['Avg_Rating'], i])
+        books_array.append([row['Book'], row['Author'], row['Description'], row['Genres'], row['Avg_Rating'], i])
 
     return books_array
 
+
+# Function to get a complete list of genres based on choices
+allGenres = []
+def getGenres(choices: list):
+    for j in choices:
+        genresForBook = books_array[j][3]
+        for genre in genresForBook:
+            print(genre)
+            # allGenres.append(genre)
+    
+    print(genresForBook)
+
+firstBookCall()
+getGenres([0])
+
+
+# Function to generate recommendations based on array recieved from HTML
