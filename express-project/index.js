813b0730b1277f84de946828acb12bd3ba6a18c1
ayushmish605 <mishraayush605@gmail.com>
2024-03-24 11:13:17 -0400

    last commit

--- express-project/index.js
+++ express-project/index.js
@@ -16,12 +16,3 @@ var params = {
     middleware: [function(req, res, next) { next(); }] // Takes an array of Connect-compatible middleware that are injected into the server middleware stack
 }
 liveServer.start(params);
-
-// app.post('/home', function(req, res) {
-//     request('http://127.0.0.1:5000/flask', function (error, response, body) {
-//         console.error('error:', error); // Print the error
-//         console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
-//         console.log('body:', body); // Print the data received
-//         // res.send(body); //Display the response on the website
-//       });
-// });
