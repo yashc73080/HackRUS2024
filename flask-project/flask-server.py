b9bb3d6bc84735112adab154329a5473f5a664c6
ayushmish605 <mishraayush605@gmail.com>
2024-03-24 11:23:41 -0400

    yay

--- flask-project/flask-server.py
+++ flask-project/flask-server.py
@@ -1,29 +1,3 @@
-# from flask import Flask, request, jsonify
-# from RecommendBooks import *
-
-# app = Flask(__name__)
-
-# @app.route('/flask', methods=['GET'])
-# def index():
-#     return "Flask server"
-
-# @app.route('/firstCall', methods=['POST'])
-# def handle_post_request_1():
-#     reccs = request.data.decode('utf-8').split(',')
-#     print(reccs)
-#     return reccs
-
-# @app.route('/recommend', methods=['POST'])
-# def handle_post_request_2():
-#     choices = request.data.decode('utf-8').split(',')
-#     genres = getGenres(choices)
-#     reccs = getBookRecs(genres)
-#     print(reccs)
-#     return reccs
-
-# if __name__ == "__main__":
-#     app.run(port=5000, debug=True)
-
 from flask import Flask, request, jsonify
 from flask_cors import CORS
 import numpy as np
